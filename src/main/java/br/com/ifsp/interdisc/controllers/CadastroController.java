d7b5c01439a716a1dab06364c92232ad0e4e8a36
Aluno <j7fernandes@gmail.com>
2019-05-10 22:08:32 -0300

    Adicionando bcincrypter

--- src/main/java/br/com/ifsp/interdisc/controllers/CadastroController.java
+++ src/main/java/br/com/ifsp/interdisc/controllers/CadastroController.java
@@ -2,6 +2,8 @@ package br.com.ifsp.interdisc.controllers;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,10 +14,17 @@ import br.com.ifsp.interdisc.models.Usuario;
 @Controller
 @RequestMapping("/cadastro")
 public class CadastroController {
-
+	
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Autowired
+	public CadastroController(BCryptPasswordEncoder bCryptPasswordEncoder) {
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
+	
     @GetMapping
     public String cadastro(){
-        return "cadastro";
+        return "view/pages/samples/register";
     }
     
     @PostMapping
