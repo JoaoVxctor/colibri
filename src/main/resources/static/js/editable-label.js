41915c7f40c229b36e56cd43aadbbbe9b951ee33
Aluno <matheusuzumaki26@gmail.com>
2019-06-04 22:02:44 -0300

    tela do projeto view

---
+++ src/main/resources/static/js/editable-label.js
@@ -0,0 +1,20 @@
+$('.clickMe').click(function () {
+    "use strict";
+    $(this).hide();
+    $('#' + $(this).attr('for'))
+                    .val($(this).text())
+                    .toggleClass("form-control")
+                    .show()
+                    .focus();
+});
+
+$('.blur').blur(function () {
+    "use strict";
+    $(this)
+        .hide()
+        .toggleClass("form-control");
+    var myid = (this).id;
+    $('span[for=' + myid + ']')
+        .text($(this).val())
+        .show();
+});
